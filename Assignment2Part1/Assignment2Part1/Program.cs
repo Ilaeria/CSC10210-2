ece8a8a85d28a1b9f2fefda1ed1f32378f85f43d
Jen Doherty <Ilaeria@users.noreply.github.com>
2015-05-06 18:47:54 +1000

    Removing Kai's comments

--- Assignment2Part1/Assignment2Part1/Program.cs
+++ Assignment2Part1/Assignment2Part1/Program.cs
@@ -16,8 +16,7 @@ namespace Assignment2Part1
         {
             //Create and initialise student objects
             StudentUnit student1Unit = new StudentUnit(12345678, "Bobson", "Bob", "CSC10210");
-            StudentGrade student1Grade = new StudentGrade(student1Unit.StudentID, 
-                student1Unit.LastName, student1Unit.FirstNames,"A");
+            StudentGrade student2Grade = new StudentGrade(87654321, "Jonson", "Jon", "HD");
 
             StudentDisplay studentDisplay = new StudentDisplay();
 
@@ -26,15 +25,18 @@ namespace Assignment2Part1
             studentDisplay.PrintFamilyName(student1Unit);
             studentDisplay.PrintOtherNames(student1Unit);
 
-            studentDisplay.PrintStudentIdentifier(student1Grade);
-            studentDisplay.PrintFamilyName(student1Grade);
-            studentDisplay.PrintOtherNames(student1Grade);
+            studentDisplay.PrintStudentIdentifier(student2Grade);
+            studentDisplay.PrintFamilyName(student2Grade);
+            studentDisplay.PrintOtherNames(student2Grade);
 
             string student1string = studentDisplay.ReturnStudent(student1Unit);
             Console.WriteLine(student1string);
 
+            string student2string = studentDisplay.ReturnStudent(student2Grade);
+            Console.WriteLine(student2string);
+
             //Check if student ID matches given int
-            Console.WriteLine("Student's ID matches argument: {0}", studentDisplay.CheckID(student1Unit,student1Grade));
+            Console.WriteLine("Student's ID matches argument: {0}", studentDisplay.CheckID(student1Unit,student2Grade));
 
             Console.ReadLine();
         }
--- Assignment2Part1/Assignment2Part1/StudentDisplay.cs
+++ Assignment2Part1/Assignment2Part1/StudentDisplay.cs
@@ -8,37 +8,14 @@ namespace Assignment2Part1
 {
     //Various methods for returning different student attributes
     
-    //Kai: Student Displkay is independent and not implementing the interface, removed and removed its properties
     public class StudentDisplay
-    {
-        //Properties
-        //Kai: public int StudentID { get; set; }
-        //Kai: public string LastName { get; set; }
-        //Kai: public string FirstNames { get; set; }
-        
+    {        
         //constructor
-        // Kai: emptied constructor
         public StudentDisplay()
         {
         }
 
-        //Interface methods
-        /* Kai:
-        public int StudentIdentifier()
-        {
-            return StudentID;
-        }
-
-        public string FamilyName()
-        {
-            return LastName;
-        }
-
-        public string OtherNames()
-        {
-            return FirstNames;
-        }*/
-
+        //Methods for printing different student attributes
         public void PrintStudentIdentifier(IStudent student)
         {
             Console.WriteLine("Student ID: {0}", student.StudentIdentifier());
@@ -53,7 +30,8 @@ namespace Assignment2Part1
         {
             Console.WriteLine("Student Other Names: {0}", student.OtherNames());
         }
-
+        
+        //Returns a formatted string of student attributes
         public string ReturnStudent(IStudent student)
         {
             string output = ("Student ID: " + student.StudentIdentifier() + ", Student Family Name: "
@@ -61,6 +39,7 @@ namespace Assignment2Part1
             return output;
         }
 
+        //Checks an ID matches another ID
         public bool CheckID(IStudent student1, IStudent student2)
         {
             if (student1.StudentIdentifier() == student2.StudentIdentifier())
@@ -72,39 +51,5 @@ namespace Assignment2Part1
                 return false;
             }
         }
-
-        /* Kai: public void PrintStudentIdentifier()
-        {
-            Console.WriteLine("Student ID: {0}", StudentID);
-        }
-
-        public void PrintFamilyName()
-        {
-            Console.WriteLine("Student Family Name: {0}", LastName);
-        }
-
-        public void PrintOtherNames()
-        {
-            Console.WriteLine("Student Other Names: {0}", FirstNames);
-        }
-
-        public string ReturnStudent()
-        {
-            string student = ("Student ID: " + StudentID + ", Student Family Name: " 
-                + LastName + ", Student Other Names: " + FirstNames);
-            return student;
-        }
-
-        public bool CheckID(int studentIdent, int checkInt)
-        {
-            if (studentIdent == checkInt)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }*/
     }
 }
