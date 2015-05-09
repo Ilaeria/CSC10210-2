29499425f805a5bfdc4feb87c5949f2145fbb366
Jen Doherty <Ilaeria@users.noreply.github.com>
2015-05-09 17:47:18 +1000

    Part 3a - implementing exception class

--- Assignment2Part3/Assignment2Part3/Money.cs
+++ Assignment2Part3/Assignment2Part3/Money.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Assignment2Part3
 {
-    class Money
+    public class Money
     {
+        //Properties
+        public int Dollars { get; set; }
+        public int Cents { get; set; }
+
+        //Constructors
+        public Money(int d, int c)
+        {
+            Dollars = d;
+            try
+            {
+                if (c >= 0 && c <= 99)
+                {
+                    Cents = c;
+                }
+                else
+                {
+                    throw new MoneyException(0, "Cents must be between 0 and 99");
+                }
+            }
+            catch (MoneyException centsException)
+            {
+                Console.WriteLine(centsException.Message);
+                Console.ReadLine();
+            }
+        }
+
+        public Money(int d)
+        {
+            Dollars = d;
+        }
+
+        public override string ToString()
+        {
+            if (Cents < 10)
+            {
+                return string.Format("${0}.0{1}", Dollars, Cents);
+            }
+            else
+            {
+                return string.Format("${0}.{1}", Dollars, Cents);
+            }
+        }
     }
 }
---
+++ Assignment2Part3/Assignment2Part3/MoneyExcception.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2Part3
+{
+    //Handle various formatting exceptions with the Money class
+    public class MoneyException : Exception
+    {
+        public int code = 0;
+        public string error = "";
+
+        //default constructor
+        public MoneyException()
+            : base("Your money is not in the correct format.")
+        {
+
+        }
+
+        //constructor with error message
+        public MoneyException(int errorCode, string errorMessage)
+            : base(errorMessage)
+        {
+            this.code = errorCode;
+            this.error = errorMessage;
+        }
+    }
+}
