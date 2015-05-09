fc465b70ee7f4ac88800fcb98cd5e77c328f5930
Jennifer Doherty <dohertyja@gmail.com>
2015-05-09 17:47:18 +1000

    Part 3a - implementing exception class

--- Assignment2Part3/Assignment2Part3/Program.cs
+++ Assignment2Part3/Assignment2Part3/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Author: Jennifer Doherty Student ID 22223407
+//Title: CSC10210 S1 2015 Assignment 2 Part 1
+//Date: 02/05/2015
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,15 @@ namespace Assignment2Part3
     {
         static void Main(string[] args)
         {
+            Money cost = new Money(23, 17);
+            Money bill = new Money(100);
+
+            Console.WriteLine(cost);
+            Console.WriteLine(bill);
+
+            Money invalid = new Money(44, 123);
+
+            Console.ReadLine();
         }
     }
 }
