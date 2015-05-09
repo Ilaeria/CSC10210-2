2125e93cee8ac740a16107f9d84504178731f0ed
Jennifer Doherty <dohertyja@gmail.com>
2015-05-09 14:07:03 +1000

    Initial creation of part 2

---
+++ Assignment2Part2/Assignment2Part2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Author: Jennifer Doherty Student ID 22223407
+//Title: CSC10210 S1 2015 Assignment 2 Part 1
+//Date: 02/05/2015
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
